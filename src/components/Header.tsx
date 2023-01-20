68d5568aaf61d7bdf76a74cae868b68ee11e2203
unknown <brenomoraes97@yahoo.com>
2023-01-20 03:09:36 -0300

    feat: created component

---
+++ src/components/Header.tsx
@@ -0,0 +1,18 @@
+import LogoImage from "../assets/logo.svg";
+import { Plus } from "phosphor-react";
+const Header = () => {
+  return (
+    <div className="w-full max-w-3xl mx-auto flex items-center justify-between">
+      <img src={LogoImage} alt="Habits" />
+      <button
+        className="border border-violet-500 font-semibold rounded-lg px-6 py-4 flex items-center gap-3 hover:border-violet-300"
+        type="button"
+      >
+        <Plus size={20} className="text-violet-500" />
+        Novo Hábito
+      </button>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/components/SummaryTable.tsx
@@ -0,0 +1,38 @@
+import HabitDay from "./HabitDay";
+const weekDays = ["D", "S", "T", "Q", "Q", "S", "S"];
+import { generateDatesFromYearBeginning } from "../utils/generate-dates-from-year-neginning";
+const SummaryDates = generateDatesFromYearBeginning();
+const minimunSummaryDatesSize = 18 * 7;
+const amountOfDaysToFill = minimunSummaryDatesSize - SummaryDates.length;
+const SummaryTable = () => {
+  return (
+    <div className="w-full flex">
+      <div className="grid grid-rows-7 grid-flow-row gap-3">
+        {weekDays.map((weekDay, i) => (
+          <div
+            key={`${weekDay} - ${i}`}
+            className="text-zinc-400 text-xl h-10 w-10 font-bold flex items-center justify-center"
+          >
+            {weekDay}
+          </div>
+        ))}
+      </div>
+
+      <div className="grid grid-rows-7 grid-flow-col gap-3">
+        {SummaryDates.map((date) => (
+          <HabitDay key={date.toString()} />
+        ))}
+
+        {amountOfDaysToFill > 0 &&
+          Array.from({ length: amountOfDaysToFill }).map((_, i) => (
+            <div
+              key={i}
+              className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed"
+            />
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default SummaryTable;
